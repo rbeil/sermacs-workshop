9050719665a0e5aa2076b0129c50451129a53de0
Randi Beil <rbeil1@vols.utk.edu>
2018-10-31 14:07:32 -0400

    Added functions and error messages

---
+++ molssi_math/string_util.py
@@ -0,0 +1,37 @@
+"""
+string_util.py
+Sample repository for MolSSI Workshop at SERMACS
+
+Misc. string processing funtions
+"""
+
+def title_case(sentence):
+	"""
+	Convert string to title case
+
+	Parameters
+	------------
+	sentence: string
+	  Sentence to be converted to title case
+
+	Returns:
+	------------
+	ret: string
+	  Input string in title case
+
+	Example
+	-----------
+	>>>> title_case('ThIs iS a StrIng tO be ConVerted')
+	  This Is A String To Be Converted
+	"""
+	if not isinstance(sentence, str):
+	   raise TypeError('Input must be type string')
+	
+	if len(s)==0:
+	   raise IndexError('Cannot apply title function to empty string.')
+	
+	sentence.lower()
+	small=sentence.split()
+	small = [word.capitalize() for word in small]
+
+	return join(small)
